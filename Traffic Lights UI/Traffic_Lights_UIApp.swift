7177e8f25cede479f9289060ecc92bffbdcb0bea
Dmitry Pavlov <perr07@yandex.ru>
2021-03-25 09:36:29 +0300

    Initial Commit

---
+++ Traffic Lights UI/Traffic_Lights_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_Lights_UIApp.swift
+//  Traffic Lights UI
+//
+//  Created by Pandos on 25.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_Lights_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
